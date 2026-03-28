74d8d3ea94cb9926927d50240c9b816a0a549938
tnamit <lctrungnam25@gmail.com>
2026-03-28 16:18:07 +0700

    oke

---
+++ ManGnurt.Consoleapp/ManGnurt.DataAccess/Employeer_Manager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManGnurt.DataAccess
+{
+     public static class Employeer_Manager
+    {
+        public static int Employeer_Insert(string EmpLoyeerCode, string EmpLoyeerName, DateTime StartDate)
+        {
+            var ketqua = 0;
+            try
+            {
+                if (ManGnurt.Common.ValidatorInput.IsValidString(EmpLoyeerCode))
+                {
+
+                }
+                    
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return ketqua;
+        }
+    }
+}
